e3e630ee1cc51fa75350740b44b7e354c351f026
kencal <kenadjei20@gmail.com>
2023-04-04 22:52:41 +0000

    updating input & button box-shadow in js and css

--- signup.js
+++ signup.js
@@ -1,16 +1,20 @@
 //Event listeners
-document.getElementById("button").addEventListener("mouseover",button_color)
-document.getElementById("button").addEventListener("mouseout",button_color2)
+document.getElementById("button").addEventListener("mouseover", button_color);
+document.getElementById("button").addEventListener("mouseout", button_color2);
 
 
-//functions
+functions;
 function button_color() {
-  document.getElementById("button").style.backgroundColor="yellow"
-  document.getElementById("button").style.color="black"
+  document.getElementById("button").style.backgroundColor = "yellow";
+  document.getElementById("button").style.color = "black";
+  document.getElementById("button").style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
 }
 
 function button_color2() {
-    document.getElementById("button").style.color="white"
-    document.getElementById("button").style.backgroundColor=" rgba(61, 184, 58, 1)"
+  document.getElementById("button").style.color = "white";
+  document.getElementById("button").style.backgroundColor =
+    " rgba(61, 184, 58, 1)";
+  document.getElementById("button").style.boxShadow = "none";
 }
 
