80314c793b366f86cada8577f27235059582f240
kencal <kenadjei20@gmail.com>
2023-04-08 18:28:03 +0000

    update of input field css&js

--- signup.js
+++ signup.js
@@ -1,6 +1,18 @@
 //Event listeners
 document.getElementById("button").addEventListener("mouseover", button_color);
 document.getElementById("button").addEventListener("mouseout", button_color2);
+document.getElementsByClassName("input")[0].addEventListener("mouseover",inputshadow0a);
+document.getElementsByClassName("input")[0].addEventListener("mouseout", inputshadow0b);
+document.getElementsByClassName("input")[1].addEventListener("mouseover",inputshadow1a);
+document.getElementsByClassName("input")[1].addEventListener("mouseout", inputshadow1b);
+document.getElementsByClassName("input")[2].addEventListener("mouseover",inputshadow2a);
+document.getElementsByClassName("input")[2].addEventListener("mouseout", inputshadow2b);
+document.getElementsByClassName("input")[3].addEventListener("mouseover",inputshadow3a);
+document.getElementsByClassName("input")[3].addEventListener("mouseout", inputshadow3b);
+document.getElementsByClassName("input")[4].addEventListener("mouseover",inputshadow4a);
+document.getElementsByClassName("input")[4].addEventListener("mouseout", inputshadow4b);
+document.getElementsByClassName("input")[5].addEventListener("mouseover",inputshadow5a);
+document.getElementsByClassName("input")[5].addEventListener("mouseout", inputshadow5b);
 
 
 functions;
@@ -18,3 +30,55 @@ function button_color2() {
   document.getElementById("button").style.boxShadow = "none";
 }
 
+
+function inputshadow0a() {
+  document.getElementsByClassName("input")[0].style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
+}
+function inputshadow0b() {
+  document.getElementsByClassName("input")[0].style.boxShadow="none"
+  
+}
+
+function inputshadow1a() {
+  document.getElementsByClassName("input")[1].style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
+}
+function inputshadow1b() {
+  document.getElementsByClassName("input")[1].style.boxShadow="none"
+  
+}
+
+function inputshadow2a() {
+  document.getElementsByClassName("input")[2].style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
+}
+function inputshadow2b() {
+  document.getElementsByClassName("input")[2].style.boxShadow="none"
+  
+}
+
+function inputshadow3a() {
+  document.getElementsByClassName("input")[3].style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
+}
+function inputshadow3b() {
+  document.getElementsByClassName("input")[3].style.boxShadow="none"
+  
+}
+function inputshadow4a() {
+  document.getElementsByClassName("input")[4].style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
+}
+function inputshadow4b() {
+  document.getElementsByClassName("input")[4].style.boxShadow="none"
+  
+}
+function inputshadow5a() {
+  document.getElementsByClassName("input")[5].style.boxShadow =
+    " 10px 10px 12px 0px rgba(0, 0, 0, 0.25)";
+}
+function inputshadow5b() {
+  document.getElementsByClassName("input")[5].style.boxShadow="none"
+  
+}
